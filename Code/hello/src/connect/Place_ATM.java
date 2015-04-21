87504f6b18b720204d7c35bd83c07235117e075d
dinhvanvu94@gmail.com <dinhvanvu94@gmail.com@53819e1a-876e-f0df-0ef9-3140377865f6>
2015-04-21 17:28:34 +0000

    Update Place ATM

--- Code/hello/src/connect/Place_ATM.java
+++ Code/hello/src/connect/Place_ATM.java
@@ -19,6 +19,114 @@ import com.google.api.client.json.jackson.JacksonFactory;
 @SuppressWarnings("deprecation")
 public class Place_ATM {
 
-	
+	/** Global instance of the HTTP transport. */
+	private static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
+
+	// Google API Key
+	private static final String API_KEY = "AIzaSyB42A0MqBlXb3dEPSk9saKUhDq7OQ6GAMQ"; // place
+																						// your
+																						// API
+																						// key
+																						// here
+
+	// Google Places serach url's
+	private static final String PLACES_SEARCH_URL = "https://maps.googleapis.com/maps/api/place/search/json?";
+	private static final String PLACES_TEXT_SEARCH_URL = "https://maps.googleapis.com/maps/api/place/search/json?";
+	private static final String PLACES_DETAILS_URL = "https://maps.googleapis.com/maps/api/place/details/json?";
+
+	private double _latitude;
+	private double _longitude;
+	private double _radius;
+
+	/**
+	 * Searching places
+	 * 
+	 * @param latitude
+	 *            - latitude of place
+	 * @params longitude - longitude of place
+	 * @param radius
+	 *            - radius of searchable area
+	 * @param types
+	 *            - type of place to search
+	 * @return list of places
+	 * */
+	public PlacesList search(double latitude, double longitude, double radius,
+			String types, String name) throws Exception {
+
+		this._latitude = latitude;
+		this._longitude = longitude;
+		this._radius = radius;
+
+		try {
+
+			HttpRequestFactory httpRequestFactory = createRequestFactory(HTTP_TRANSPORT);
+			HttpRequest request = httpRequestFactory
+					.buildGetRequest(new GenericUrl(PLACES_SEARCH_URL));
+			request.getUrl().put("key", API_KEY);
+			request.getUrl().put("location", _latitude + "," + _longitude);
+			request.getUrl().put("radius", _radius); // in meters
+
+			request.getUrl().put("sensor", "false");
+			if (types != null) {
+				request.getUrl().put("types", types);
+			}
+			if (name != null) {
+				request.getUrl().put("name", name);
+			}
+
+			PlacesList list = request.execute().parseAs(PlacesList.class);
+			// Check log cat for places response status
+			Log.d("Places Status", "" + list.status);
+			return list;
+
+		} catch (HttpResponseException e) {
+			Log.e("Error:", e.getMessage());
+			return null;
+		}
+
+	}
+
+	/**
+	 * Searching single place full details
+	 * 
+	 * @param refrence
+	 *            - reference id of place - which you will get in search api
+	 *            request
+	 * */
+	public PlaceDetails getPlaceDetails(String reference) throws Exception {
+		try {
+
+			HttpRequestFactory httpRequestFactory = createRequestFactory(HTTP_TRANSPORT);
+			HttpRequest request = httpRequestFactory
+					.buildGetRequest(new GenericUrl(PLACES_DETAILS_URL));
+			request.getUrl().put("key", API_KEY);
+			request.getUrl().put("reference", reference);
+			request.getUrl().put("sensor", "false");
+
+			PlaceDetails place = request.execute().parseAs(PlaceDetails.class);
+
+			return place;
+
+		} catch (HttpResponseException e) {
+			Log.e("Error in Perform Details", e.getMessage());
+			throw e;
+		}
+	}
+
+	/**
+	 * Creating http request Factory
+	 * */
+	public static HttpRequestFactory createRequestFactory(
+			final HttpTransport transport) {
+		return transport.createRequestFactory(new HttpRequestInitializer() {
+			public void initialize(HttpRequest request) {
+				GoogleHeaders headers = new GoogleHeaders();
+				headers.setApplicationName("");
+				request.setHeaders(headers);
+				JsonHttpParser parser = new JsonHttpParser(new JacksonFactory());
+				request.addParser(parser);
+			}
+		});
+	}
 
 }
